321e6d1773d5c2eb888496138c06cb6fd599603d
Xiang Zhang <zxteloiv@gmail.com>
2015-10-25 10:35:19 +0800

    add navigation bar for the site

--- prs/views/templates/header.php
+++ prs/views/templates/header.php
@@ -20,8 +20,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <!--
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
     -->
-    <link rel="stylesheet" href="prs/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="prs/assets/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
 </head>
 <body>
 
+<nav class="navbar navbar-default navbar-static-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/prs">首页</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+          <li <?php if ($active_title == 'query') echo 'class="active"'; ?>><a href="query">查询题目</a></li>
+          <li <?php if ($active_title == 'add') echo 'class="active"'; ?>><a href="add">增加题目</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
