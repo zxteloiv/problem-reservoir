00515fed3d5f13871d77a234ae048d0ee0d44e84
Xiang Zhang <zxteloiv@gmail.com>
2015-10-28 21:15:59 +0800

    add deletion operation for problems

--- prs/controllers/problem.php
+++ prs/controllers/problem.php
@@ -3,11 +3,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Problem extends CI_Controller {
 
-	public function index() {
+    function __construct() {
+        parent::__construct();
         $this->get = $this->input->get();
         $this->post = $this->input->post();
-        $this->file = $_FILES;
+    }
+
+	public function index() {
         
         return;
 	}
+
+    public function byID($pid) {
+        return;
+    }
+
+    public function byIDs($list) {
+        echo urldecode($list);
+    }
+
+    public function create() {
+    }
+
+    public function del() {
+        $this->load->model('ProblemOp');
+        $this->ProblemOp->doDeletion($this->post);
+    }
 }
--- prs/models/problemOp.php
+++ prs/models/problemOp.php
@@ -1,9 +1,13 @@
 <?php
-class Problem extends CI_Model {
+class ProblemOp extends CI_Model {
+
+    const DELETE_PID_NOT_FOUND = 5001;
+    const DELETION_FAILED = 5002;
 
     function __construct() {
         parent::__construct();
         $this->db = $this->load->database('prs', true);
+        $this->load->helper('utils');
     }
 
     function getProblemByID($pid){
@@ -45,5 +49,27 @@ class Problem extends CI_Model {
     }
 
     function updateProblemByID($pid, $param) {
+    }
+
+    function doDeletion($param) {
+        if (!isset($param['pid'])) {
+            echo outputError(self::DELETE_PID_NOT_FOUND, 'unknown key to delete');
+            return;
+        }
+        $pid = $param['pid'];
+
+        $this->db->where('problem_id', $pid, true);
+        $rtn = $this->db->delete('problems');
+
+        if ($rtn && $this->db->affected_rows() > 0) {
+            echo json_encode(array(
+                'errno' => 0,
+                'errmsg' => 'delete successfully'
+            ));
+        } else {
+            echo outputError(self::DELETION_FAILED, 'failed to execute deletion');
+        }
+
+        return;
     }
 }
